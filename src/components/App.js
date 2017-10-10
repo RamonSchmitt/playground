97a1a5d4467b3990faebf4418da33ec5ea47cf8c
RamonSchmitt <ramon.schmitt@yahoo.com>
2017-10-10 12:50:06 +0200

    ?

---
+++ src/components/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      orders: [],
+    };
+  }
+
+  componentWillMount() {
+    const orders = JSON.parse(localStorage.getItem("orders"));
+
+    this.setState({ orders });
+  }
+
+  addToOrder() {
+    console.log(this.props);
+  }
+
+  removeFromOrder() {
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+     <div>
+        <h1>Time to play</h1>
+        <button onClick={this.addToOrder.bind(this)}>add to order</button>
+        <button onClick={this.removeFromOrder}>remove</button>
+        <p>{this.state.orders}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
