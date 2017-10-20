128dd517985a17c0be8abdd8ff0a0a18f101b515
RamonSchmitt <ramon.schmitt@yahoo.com>
2017-10-20 13:59:26 +0200

    final

--- src/components/App.js
+++ src/components/App.js
@@ -15,23 +15,39 @@ class App extends Component {
     this.setState({ orders });
   }
 
-  addToOrder() {
-    this.setState(prevState => ({
-      orders: prevState.orders
-    }));
+  addToOrder = () => {
+    this.setState({
+      orders: [...this.state.orders, this.props.dish]
+    });
+
+    localStorage.setItem("orders", JSON.stringify(this.state.orders));
   }
 
-  removeFromOrder() {
+  removeFromOrder = () => {
+    const { orders } = this.state;
+    const index = orders.indexOf("nasi");
+    orders.splice(index, 1);
+
+    this.setState({ orders });
+    
+    localStorage.setItem("orders", JSON.stringify(this.state.orders));
+  }
+
+  renderOrderList() {
+    const { orders } = this.state;
+
+    return orders.map( order => {
+      return <li>{order}</li>;
+    })
   }
 
   render() {
-    console.log(this.props);
     return (
      <div>
         <h1>Time to play</h1>
-        <button onClick={this.addToOrder.bind(this)}>add to order</button>
+        <button onClick={this.addToOrder}>add to order</button>
         <button onClick={this.removeFromOrder}>remove</button>
-        <p>{this.state.orders}</p>
+        <ol>{this.renderOrderList()}</ol>
       </div>
     );
   }
